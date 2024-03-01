976a9a4eec082a93f16e2a5b0029187903ce6d58
kirill yartsev <115335212+yunhkirill@users.noreply.github.com>
2024-03-01 19:53:46 +0300

    Add SphericalPoint class

---
+++ lib/spherical_point.cpp
@@ -0,0 +1,60 @@
+#include "spherical_point.h"
+#include <iostream>
+
+// TODO: This class does not take in mind Earth curvature
+// aka R_earth >> R
+
+SphericalPoint::SphericalPoint(const AbstractPoint &center,
+                               const AbstractPoint &position)
+    : center{center} {
+  double dx = position.get_x() - center.get_x();
+  double dy = position.get_y() - center.get_y();
+  double dh = position.get_h() - center.get_h();
+  R = sqrt(dx * dx + dy * dy + dh * dh);
+  double d = sqrt(dx * dx + dy * dy);
+  phi = atan(dh / d);
+  // Correctly compute azimuth for all quarters
+  if (dx == 0) {
+    azimuth = (dy >= 0) ? M_PI_2 : -M_PI_2;
+    return;
+  }
+  azimuth = atan(dy / dx);
+  azimuth += dx < 0 ? M_PI : 0;
+  azimuth += (dx > 0 && dy < 0) ? 2 * M_PI : 0;
+}
+
+double SphericalPoint::get_x() const {
+  double cos_azimuth =
+      (cos(azimuth) <= 1e-10 && cos(azimuth) > 0) ? 0 : cos(azimuth);
+  double cos_phi = (cos(phi) <= 1e-10 && cos(phi) > 0) ? 0 : cos(phi);
+  return R * cos_azimuth * cos_phi;
+}
+
+double SphericalPoint::get_y() const {
+  double sin_azimuth =
+      (sin(azimuth) <= 1e-10 && sin(azimuth) > 0) ? 0 : sin(azimuth);
+  double cos_phi = (cos(phi) <= 1e-10 && cos(phi) > 0) ? 0 : cos(phi);
+  return R * sin_azimuth * cos_phi;
+}
+
+double SphericalPoint::get_h() const {
+  double sin_phi = (sin(phi) <= 1e-10 && sin(phi) > 0) ? 0 : sin(phi);
+  return R * sin_phi;
+}
+
+double SphericalPoint::get_d() const {
+  double cos_phi = (cos(phi) <= 1e-10 && cos(phi) > 0) ? 0 : cos(phi);
+  return R * cos_phi;
+}
+
+void SphericalPoint::change_h(double new_h) {
+  double prev_d = get_d();
+  phi = atan(new_h / prev_d);
+  R = sqrt(new_h * new_h + prev_d * prev_d);
+}
+
+void SphericalPoint::change_d(double new_d) {
+  double prev_h = get_h();
+  phi = atan(prev_h / new_d);
+  R = sqrt(prev_h * prev_h + new_d * new_d);
+}
